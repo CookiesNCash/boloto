6e19b3cfe7e1a903d571ea502aaa2afe1d3b3d09
bitsen03 <bitsen03@mail.ru>
2024-06-12 01:55:32 +0300

    add registration

---
+++ client/src/app/welcome/components/DropDownDate.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Dropdown } from 'react-bootstrap';
+
+export default function DropDownDate ({ arr, name }) {
+    return (
+        <div className="dropdown-container">
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    {name}
+                </Dropdown.Toggle>
+                <Dropdown.Menu className='dropdown-menu-scroll'>
+                    {arr.map((el, indx) => (
+                        <Dropdown.Item key={indx} href={`#/action-${indx}`}>
+                            {el}
+                        </Dropdown.Item>
+                    ))}
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    )
+}
