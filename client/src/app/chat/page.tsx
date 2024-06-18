76b7fb96456ced8d10fd2065ba3b959d3fde5f30
SergeiKiss <kiselev_s.d.98@mail.ru>
2024-06-18 14:12:49 +0300

    implement chat to server; add socket.io example to client

---
+++ client/src/app/chat/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from 'react';
+import io from 'socket.io-client';
+
+export default () => {
+    const [message, setMessage] = useState('');
+    const socket = io('http://localhost:80');
+    const handleSubmit = () => {
+        socket.emit('message', {
+            data: message,
+        });
+    };
+    socket.on('message', ({ data }) => {
+        console.log(data)
+    })
+    return (
+        <>
+            <input onChange={(e) => {
+                setMessage(e.target.value)
+            }}/>
+            <button onClick={handleSubmit}>send</button>
+        </>
+    );
+};
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,8 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
   imports: [AuthModule, UserModule, PostModule, PrismaModule],
+  providers: [ChatGateway],
 })
 export class AppModule {}
